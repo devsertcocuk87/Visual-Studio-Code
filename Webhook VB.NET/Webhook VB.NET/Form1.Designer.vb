69f8b209401e68472f15c4c046b8dd587070f9ed
devsertcocuk87 <102773393+devsertcocuk87@users.noreply.github.com>
2022-04-01 03:56:57 +0300

    Add files via upload

---
+++ Webhook VB.NET/Webhook VB.NET/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txt_message = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txt_message
+        '
+        Me.txt_message.Location = New System.Drawing.Point(78, 23)
+        Me.txt_message.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txt_message.Name = "txt_message"
+        Me.txt_message.Size = New System.Drawing.Size(388, 22)
+        Me.txt_message.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(201, 53)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(106, 36)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Send"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Message:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(349, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(131, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "www.sertcocuk87.org"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(148, 16)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Created by Sertcocuk87"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(492, 130)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txt_message)
+        Me.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Dev.Sertcocuk87 | Discord WebHook Send Message"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt_message As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Webhook VB.NET/Webhook VB.NET/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Net
+Imports System
+Imports System.Collections.Generic
+Imports System.Collections.Specialized
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Text.RegularExpressions
+Public Class Form1
+
+    Public Shared Function Post(ByVal uri As String, ByVal pairs As NameValueCollection) As Byte()
+        Using webClient As WebClient = New WebClient()
+            Return webClient.UploadValues(uri, pairs)
+        End Using
+    End Function
+    Public Shared Sub webhookgonder(ByVal Url As String, ByVal avatar_url As String, ByVal msg As String, ByVal Username As String)
+        Post(Url, New System.Collections.Specialized.NameValueCollection() From {
+        {"avatar_url", avatar_url},
+        {"username", Username},
+        {"content", msg}
+    })
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        webhookgonder("https://webhookurl", "https://avatarurl", txt_message.Text, "DevSertcocuk87")
+    End Sub
+
+    '//////////// Developer's by Sertcocuk87 www.sertcocuk87.org /////////////
+End Class
